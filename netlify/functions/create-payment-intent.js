bb335de171616c2b29bdf0963a508012e449f7d4
kevin1999721 <kevin1999721@gmail.com>
2022-09-04 14:11:28 +0800

    build serverless functions with netlify + stripe

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_PRIVATE_KEY);
+
+exports.handler = async req => {
+	try {
+		const { amount } = JSON.parse(req.body);
+
+		const paymentIntent = await stripe.paymentIntents.create({
+			amount,
+			currency: 'usd',
+			payment_method_types: ['card'],
+		});
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({ paymentIntent }),
+		};
+	} catch (error) {
+		console.log({ error });
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ error }),
+		};
+	}
+};
